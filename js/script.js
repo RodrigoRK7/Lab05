9f51228c52b3a7388e7baa8d773c4e1848742f13
RodrigoRK7 <a00824488@itesm.mx>
2021-09-26 17:23:59 -0500

    Lab 05

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let texto = "#todoText";
+let lista = "#todoList";
+let nombre = '[name="todo"]';
+let post = "#ButtonPost";
+let clear = "#ButtonClear";
+let mark = "#ButtonMark";
+let deleteb = "#ButtonDelete";
+
+$(post).on("click", function (event) {
+  event.preventDefault();
+  if ($(texto).val() != "") {
+    $(lista).append(
+      `<input type = "checkbox" name="todo" /> ${$(texto).val()} <br>`
+    );
+  }
+});
+
+$(clear).on("click", function () {
+  $(nombre).each(function () {
+    $(this).prop("checked", false);
+  });
+});
+
+$(mark).on("click", function () {
+  $(nombre).each(function () {
+    $(this).prop("checked", true);
+  });
+});
+
+$(deleteb).on("click", function () {
+  $(lista).html("");
+});
